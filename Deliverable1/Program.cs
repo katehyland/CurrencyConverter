a963a5085ea0354c1501c661434f16026368fd03
Kate Hyland <katehyland@quickenloans.com>
2019-08-16 21:56:03 -0400

    Pushing changes after reviewing * updating solution

--- Deliverable1/Program.cs
+++ Deliverable1/Program.cs
@@ -1,12 +1,76 @@
-﻿using System;
+﻿
+using System;
+using System.Linq;
+using System.Globalization;
 
-namespace Deliverable1
+namespace AbsoluteUnit1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            decimal decValA = 0;
+            decimal decValB = 0;
+            decimal decValC = 0;
+            bool valid = false;
+
+            while (!valid)
+            {
+                Console.WriteLine("Enter a number & press Enter: ");
+                string inputA = Console.ReadLine();
+                if (decimal.TryParse(inputA, out decValA))
+                {
+                    decValA = Math.Round(decValA, 2);
+                    valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Not a number. Try again.");
+                }
+
+                Console.WriteLine("Enter a number & press Enter again: ");
+                string inputB = Console.ReadLine();
+                if (decimal.TryParse(inputB, out decValB))
+                {
+                    decValB = Math.Round(decValB, 2);
+                    valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Not a number. Try again.");
+                }
+                Console.WriteLine("Enter a number & press Enter one more time, I swear: ");
+                string inputC = Console.ReadLine();
+                if (decimal.TryParse(inputC, out decValC))
+                {
+                    decValC = Math.Round(decValC, 2);
+                    valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Not a number. Try again.");
+                }
+            }
+
+            decimal total = decValA + decValB + decValC;
+            decimal average = Math.Round((total / 3), 2);
+
+            Console.WriteLine("The average is: " + average);
+
+
+            decimal[] array1 = { decValA, decValB, decValC };
+            Console.WriteLine("The highest number is: " + (array1.Max()));
+            Console.WriteLine("The lowest number is: " + (array1.Min()));
+
+
+            Console.WriteLine(total.ToString("C", CultureInfo.CurrentCulture));
+
+            Console.WriteLine(total.ToString("C", new CultureInfo("da-DK")));
+
+            Console.WriteLine(total.ToString("C", new CultureInfo("ja-jp")));
+
+            Console.WriteLine(total.ToString("C", new CultureInfo("th-TH")));
         }
     }
 }
